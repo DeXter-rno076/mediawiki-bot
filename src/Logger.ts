8ec18468e4d71e85dc521e9bbe8a92b39d7f864e
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-08-19 22:14:24 +0200

    fixing various bugs

--- src/Logger.ts
+++ src/Logger.ts
@@ -9,34 +9,36 @@ interface MainlogEntry {
 
 export default class Logger {
 	readonly DIR_PATH = './logs';
-	logFileName = `${this.DIR_PATH}/${Bot.taskId}.txt`;
-	mainlogFileName = `${this.DIR_PATH}/mainlog.json`;
+	logFileName = '';
+	readonly mainlogFileName = `${this.DIR_PATH}/mainlog.json`;
 
 	constructor () {
 		this.initDirStructure();
 		if (Bot.taskId === -1) {
-			console.error('something went wrong in Logger initialization. Task id still has starting value');
+			throw 'something went wrong in Logger initialization. Task id still has starting value';
 		}
 	}
 
 	initDirStructure () {
+		let logData: MainlogEntry[] = [];
 		try {
-			fs.accessSync(this.DIR_PATH, fs.constants.F_OK);
 			//accesSync throws an error if it cant reach the targeted path
-			const logData = JSON.parse(fs.readFileSync(
+			fs.accessSync(this.DIR_PATH, fs.constants.F_OK);
+
+			logData = JSON.parse(fs.readFileSync(
 				this.mainlogFileName, {encoding: 'utf8'}
 			)) as MainlogEntry[];
 			const lastTaskId = logData[logData.length - 1].id as number;
 			Bot.taskId = lastTaskId + 1;
-
-			this.addNewMainLogEntry(logData);
-
 		} catch (e) {
 			fs.mkdirSync(this.DIR_PATH);
 			fs.writeFileSync(this.mainlogFileName, '[]');
 			Bot.taskId = 0;
+			logData = [];
 		} finally {
+			this.logFileName = `${this.DIR_PATH}/${Bot.taskId}.txt`;
 			fs.writeFileSync(this.logFileName, '');
+			this.addNewMainLogEntry(logData);
 		}
 	}
 
@@ -53,9 +55,11 @@ export default class Logger {
 	//is called from one central place in class Bot
 	save (msg: LogEntry) {
 		const txt = msg.print() + '\n';
+		console.log(txt);
 		fs.appendFileSync(this.logFileName, txt);
 	}
 
+	//supposed to be called by the user
 	saveMsg (msg: string) {
 		fs.appendFileSync(this.logFileName, msg + '\n');
 	}
