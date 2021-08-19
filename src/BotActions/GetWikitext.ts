5433d60ded763e69125607dcf6ff43da4c3b0021
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-08-19 12:47:07 +0200

    adding section index setting to GetWikitext

--- src/BotActions/GetWikitext.ts
+++ src/BotActions/GetWikitext.ts
@@ -2,6 +2,8 @@ import BotAction from "./BotAction";
 import GetWikitextOptions from "../Options/GetWikitextOptions";
 import RequestHandler from "../RequestHandler";
 import BotActionReturn from "../BotActionReturn";
+import GetSections from "./GetSections";
+import GetSectionsOptions from "../Options/GetSectionsOptions";
 
 export default class GetWikitext extends BotAction {
 	opt: GetWikitextOptions;
@@ -12,8 +14,19 @@ export default class GetWikitext extends BotAction {
 	}
 
 	async exc (): Promise<BotActionReturn> {
+		if (this.opt.section !== undefined && isNaN(Number(this.opt.section))) {
+			await this.setSectionIndex();
+		}
+
 		const res = JSON.parse(await RequestHandler.get(this.opt));
 		const wikitext = res.parse.wikitext['*'] as string;
 		return new BotActionReturn(undefined, wikitext);
+	}
+
+	async setSectionIndex () {
+		const gSectionsOpts = new GetSectionsOptions(this.opt.page);
+		const gSections = new GetSections(gSectionsOpts);
+		const index = await gSections.getIndex(this.opt.section as string) as number;
+		this.opt.section = index;
 	}
 }
