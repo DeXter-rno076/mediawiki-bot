6cbbc2b15e864ef059c54face6ca306280604c4a
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-08-20 16:00:40 +0200

    adding support for other user names

--- src/Options/RevertOptions.ts
+++ src/Options/RevertOptions.ts
@@ -4,9 +4,12 @@ export default class RevertOptions extends Options {
 	user = 'self';
 	start?: Date;
 
-	constructor (start?: Date) {
+	constructor (user?: string, start?: Date) {
 		//this class is not used for the post request, so the attributes dont have to be in a particular form
 		super('revert');
+		if (user !== undefined) {
+			this.user = user;
+		}
 		if (start !== undefined) {
 			this.start = start;
 		}
