b3c63c7fdd8840ead9bb50377c95f8db2aa65038
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-09-06 16:21:54 +0200

    starting to implement error handling

--- src/BotActions/Edit.ts
+++ src/BotActions/Edit.ts
@@ -8,6 +8,7 @@ import GetSections from '../BotActions/GetSections';
 
 export default class Edit extends BotAction {
 	opt: EditOptions;
+	readonly MAX_RETRYS = 5;
 
 	constructor (opt: EditOptions) {
 		super();
@@ -19,9 +20,12 @@ export default class Edit extends BotAction {
 			await this.setSectionIndex();
 		}
 
-		const res = await RequestHandler.post(this.opt);
+		let res = await RequestHandler.post(this.opt);
 
-		//todo error handling
+		const parsedResult = JSON.parse(res);
+		if (parsedResult.error !== undefined) {
+			res = await this.handleError(parsedRes);
+		}
 
 		const logEntry = new LogEntry('edit', res);
 		return new BotActionReturn(logEntry, '');
@@ -30,7 +34,22 @@ export default class Edit extends BotAction {
 	async setSectionIndex () {
 		const getSectionsOpt = new GetSectionsOptions(this.opt.title);
 		const getSections = new GetSections(getSectionsOpt);
-		const sectionIndex = await getSections.getIndex(this.opt.section as string);
+		const sectionIndex = await getSections.getIndex(this.opt.section as string) as number;
 		this.opt.section = sectionIndex;
+	}
+
+	async handleError (parsedRes: Object): Promise<string> {
+		const eCode = parsedRes.error.code;
+		if (eCode === 'badtoken') {
+			for (let i = 0; i < this.MAX_RETRYS; i++) {
+				const res = await RequestHandler.post(this.opt);
+				const parsedRes = JSON.parse(res);
+				if (parsedRes.error === undefined) {
+					return res;
+				}
+			}
+			return 'ERROR';
+		}
+		throw 'uncommon problem or a problem I cant solve occured: ' + eCode;
 	}
 }
