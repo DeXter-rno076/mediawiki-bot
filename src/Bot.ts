2c5b487e3be45bf7b940bc8abde9a48c61e8623b
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-09-08 00:47:56 +0200

    adding documentation comments

--- src/Bot.ts
+++ src/Bot.ts
@@ -41,12 +41,26 @@ export class Bot {
 		}
 	}
 
+	/**
+	 * @returns Promise<''>
+	 */
 	login (): Promise<''> {
 		const loginOpt = new LoginOptions(Bot.username, Bot.password);
 		const login = new Login(loginOpt);
 		return this.action(login) as Promise<''>;
 	}
 
+	/**
+	 * @param title (string) page that you want to edit
+	 * @param text (string) new text of the page
+	 * @param summary (string) edit summary
+	 * @param nocreate (boolean; optional) if set to false, it's possible to create pages
+	 * @param section (string | number; optional) only edit content of this section
+	 * 
+	 * @returns Promise<''>
+	 * 
+	 * @throws BadTokenError, PageDoesNotExistError, ProtectedPageError, SectionNotFoundError, UnsolvableErrorError
+	 */
 	edit (title: string, text: string, summary: string, nocreate?: boolean, section?: string | number): Promise<''> {
 		const eOpts = new EditOptions(title, text, summary, section);
 		if (nocreate !== undefined) {
@@ -56,6 +70,16 @@ export class Bot {
 		return this.action(edit) as Promise<''>;
 	}
 
+	/**
+	 * @param from (string) old name
+	 * @param to (string) new name
+	 * @param summary (string) move summary
+	 * @param moveTalk (boolean; optional) if set to false, the talk page won't be moved
+	 * @param moveSubpages (boolean; optoinal) if set to false, potential subpages won't be moved
+	 * @param noredirect (boolean; optional) if set to false, a redirect from old name to new name will be created
+	 * 
+	 * @returns Promise<''>
+	*/
 	move (
 		from: string,
 		to: string,
@@ -72,12 +96,27 @@ export class Bot {
 		return this.action(move) as Promise<''>;
 	}
 
+	/**
+	 * @param user (string) whose edits to revert (if set to 'self', the starting time is selected via bot logs)
+	 * @param start (Date, not needed if user is set to 'self')
+	 * 
+	 * @returns Promise<''>
+	 */
 	revert (user: string, start?: Date): Promise<''> {
 		const revOpts = new RevertOptions(user, start);
 		const revert = new Revert(revOpts);
 		return this.action(revert) as Promise<''>;
 	}
 
+	/**
+	 * @param uploadType ('local' | 'remote') currently only 'remote' is possible
+	 * @param wantedName (string) name of the file ('File:' at the beginning is NOT needed)
+	 * @param comment (string) upload comment and initial file page content
+	 * @param url (string) url of file origin
+	 * @param ignoreWarnings (boolean; optional) if set to true, potential warnings by the wiki will be ignored
+	 * 
+	 * @returns Promise<''>
+	 */
 	upload (
 		uploadType: 'local' | 'remote',
 		wantedName: string,
@@ -94,7 +133,13 @@ export class Bot {
 		return this.action(upload) as Promise<''>;
 	}
 
-	
+	/**
+	 * @param category (string) category name ('Category:' at the beginning is needed)
+	 * @param ns (namespace[]; optional) array of namespaces that shall exclusevely be in the result
+	 * @param type (catMemberType; optional) which kind of pages shall be in the result (default: 'page')
+	 * 
+	 * @returns Promise<CatMember[]>
+	 */
 	getCatMembers (
 		category: string,
 		ns?: namespace[],
@@ -108,24 +153,50 @@ export class Bot {
 		return this.action(getCatMembers) as Promise<CatMember[]>;
 	}
 
+	/**
+	 * @param title (string) page name
+	 * @param section (string | number; optional) section whose templates shall be returned
+	 * 
+	 * @returns Promise<Template[]>
+	 * 
+	 * @throws SectionNotFoundError
+	 */
 	getTemplates (title: string, section?: string | number): Promise<Template[]> {
 		const getTemplatesOpts = new GetTemplatesOptions(title, section);
 		const getTemplates = new GetTemplates(getTemplatesOpts);
 		return this.action(getTemplates) as Promise<Template[]>;
 	}
 
+	/**
+	 * @param page (string) page name
+	 * @param section (string | number; optional) section whose contents to return
+	 * 
+	 * @returns Promise<string>
+	 * 
+	 * @throws PageDoesNotExistError, SectionNotFoundError, UnsolvableErrorError
+	 */
 	getWikitext (page: string, section?: string | number): Promise<string> {
 		const getWikitextOpts = new GetWikitextOptions(page, section);
 		const getWikitext = new GetWikitext(getWikitextOpts);
 		return this.action(getWikitext) as Promise<string>;
 	}
 
+	/**
+	 * @param page (string) page name
+	 * 
+	 * @returns Promise<Section[]>
+	 * 
+	 * @throws SectionNotFoundError
+	 */
 	getSections (page: string): Promise<Section[]> {
 		const getSectionsOpts = new GetSectionsOptions(page);
 		const getSections = new GetSections(getSectionsOpts);
 		return this.action(getSections) as Promise<Section[]>;
 	}
 
+	/**
+	 * @returns Logger
+	 */
 	getLogger (): Logger | null {
 		//for logger.saveMsg(txt) that's supposed to be called by the user if wanted
 		if (Bot.noLogs) {
