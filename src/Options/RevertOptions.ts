5e214af1f98e7fbdcf38b15d0fba496838bae02d
DeXter <72339348+DeXter-rno076@users.noreply.github.com>
2021-09-08 00:17:52 +0200

    switching param user to non optional

--- src/Options/RevertOptions.ts
+++ src/Options/RevertOptions.ts
@@ -4,7 +4,7 @@ export class RevertOptions extends Options {
 	user = 'self';
 	start?: Date;
 
-	constructor (user?: string, start?: Date) {
+	constructor (user: string, start?: Date) {
 		//this class is not used for the post request, so the attributes dont have to be in a particular form
 		super('revert');
 		if (user !== undefined) {
